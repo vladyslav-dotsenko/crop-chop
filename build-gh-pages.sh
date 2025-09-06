f841dd5f51b03a3f2eb80b8104cec246c2e7203d
Karponter <karponter.tarino@gmail.com>
2025-09-06 03:26:55 +0300

    feat. github pages build

---
+++ build-gh-pages.sh
@@ -0,0 +1,12 @@
+git checkout main &&
+git branch -D gh-pages &&
+git branch gh-pages &&
+git checkout gh-pages &&
+npm install &&
+npm build &&
+rm -rf node_modules public README.md AGENTS.md src package.json package-lock.json index.html eslint.config.js tsconfig.app.json tsconfig.node.json tsconfig.json tsconfig.app.json tsconfig.node.json tsconfig.json vite.config.ts build-gh-pages.sh &&
+mv build/* ./ &&
+rm -rf build &&
+git add . &&
+git commit -a -m 'release. gh-pages build' &&
+git push --force -u origin gh-pages
