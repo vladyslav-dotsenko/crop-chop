72c1d62a5746816339da86aca861be05e673aec9
Karponter <karponter.tarino@gmail.com>
2025-09-06 03:32:04 +0300

    fix. github pages build directory

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -4,9 +4,9 @@ git branch gh-pages &&
 git checkout gh-pages &&
 npm install &&
 npm build &&
-rm -rf node_modules public README.md AGENTS.md src package.json package-lock.json index.html eslint.config.js tsconfig.app.json tsconfig.node.json tsconfig.json tsconfig.app.json tsconfig.node.json tsconfig.json vite.config.ts build-gh-pages.sh &&
-mv build/* ./ &&
-rm -rf build &&
+rm -rf .vite node_modules public README.md AGENTS.md src package.json package-lock.json index.html eslint.config.js tsconfig.app.json tsconfig.node.json tsconfig.json tsconfig.app.json tsconfig.node.json tsconfig.json vite.config.ts build-gh-pages.sh &&
+mv dist/* ./ &&
+rm -rf dist &&
 git add . &&
 git commit -a -m 'release. gh-pages build' &&
 git push --force -u origin gh-pages
