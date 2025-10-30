13e89b59cbffb66b1cc9dcad56e584a832e1bb33
Karponter <karponter.tarino@gmail.com>
2025-10-30 22:25:55 +0200

    feat. simplify github pages release

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -4,9 +4,6 @@ git branch gh-pages &&
 git checkout gh-pages &&
 npm install &&
 VITE_BASE_URL='/crop-chop/' npm run build &&
-rm -rf .vite node_modules public README.md AGENTS.md src package.json package-lock.json index.html eslint.config.js tsconfig.app.json tsconfig.node.json tsconfig.json tsconfig.app.json tsconfig.node.json tsconfig.json vite.config.ts build-gh-pages.sh &&
-mv dist/* ./ &&
-rm -rf dist &&
-git add . &&
+git add ./dist &&
 git commit -a -m 'release. gh-pages build' &&
 git push --force -u origin gh-pages
